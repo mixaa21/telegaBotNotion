70048b6da91da524e10cd76f63133cb551e6c598
Mikhail <mixaa21@bk.ru>
2022-05-05 17:49:14 +0300

    My ninth commit

--- src/scenes/adminScenes/createTaskByTitle.js
+++ src/scenes/adminScenes/createTaskByTitle.js
@@ -1,7 +1,8 @@
 const {Scenes} = require("telegraf");
 const reply = require('../../../reply.json')
 const select = require("../../database/query/select");
-const split = require("../../functions/split");                                        // импортируем replay.json для сообщений
+const split = require("../../functions/split");
+const NotionService = require("../../notion/notionService");                                        // импортируем replay.json для сообщений
 
 
 module.exports = async function createTaskByTitle(client) {
@@ -15,7 +16,17 @@ module.exports = async function createTaskByTitle(client) {
     })
     exchange.on("text", async ctx => {
         ctx.session.inputTask = ctx.message.text
-        await ctx.scene.enter("createTaskByAssigne")
+        if (String(ctx.chat.id) === process.env.ADMIN_CHAT_ID) {        // если чат равен id админского чата
+            await ctx.scene.enter("createTaskByAssigne")
+        } else {
+            notion = new NotionService()
+            await notion.createTask(ctx.session.chosenClientname, ctx.session.chosenProject, ctx.session.inputTask, [{id: ctx.session.userNotionId,
+                person: {}}])
+            await ctx.reply(reply.taskIsCreated)
+            ctx.scene.enter('user')                                      // войти в сцену юзер
+        }
+
+
     })
     return exchange
 }
